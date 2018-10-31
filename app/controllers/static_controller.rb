c185c1420a52d4f55a48b2e61acfe8c58d9edcd3
EliSparow <max.peandep@gmail.com>
2018-10-31 00:45:50 +0000

    coucou j'en ai marre

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -13,6 +13,12 @@ class StaticController < ApplicationController
 
   def home
     @gossip = Gossip.all
+     puts params
+     my_name_param = params[:first_name]
+     puts my_name_param
+     @user = User.new
+    @user.first_name = params[:first_name]
+    @user.save
   end
 
   def gossip_id_page
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get '/team', to: 'static#team'
   get '/contact', to: 'static#contact'
-  get '/', to: 'static#home'
+
+  get '/', to: 'static#index'
+  post '/home', to: 'static#home'
+
+  get '/home', to: 'static#home'
   get '/gossips/:id', to: 'static#gossip_id_page', as: 'gossip'
 
 end
